332036c2693aa6d1aca76847fbf7902c72d8e1c7
JemJenn <jeremyb@aims.ac.za>
2020-01-24 16:47:11 +0200

    Gets dealer number.

--- car_spider.py
+++ car_spider.py
@@ -13,10 +13,11 @@ class CarSpider(scrapy.Spider):
             if len(make_model) == 2:
                 yield {
                     'make': make_model[0],
-                    'model': make_model[1]
+                    'model': make_model[1],
+                    'dealer_phone':car.css("a#phnum::text").get()
                 }
             else:
                 self.logger.warning("Received incorrect data: %r" % make_model)
         next_page = response.xpath("//nav/ul/li/a[@aria-label='Next']/@href").get() 
-        if next_page is not None:
-            yield response.follow(next_page, self.parse)
+        #if next_page is not None:
+        #    yield response.follow(next_page, self.parse)
