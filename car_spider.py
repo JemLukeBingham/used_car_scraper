ac1878c1806d22994de6efae8c12f74341a3f70a
chris <chrismarais@protonmail.com>
2020-01-24 14:37:54 +0200

    solve data paging issue
    Spider now iterates through successive pages using the "Next" button.
    Does is finish on the very last page though?

--- car_spider.py
+++ car_spider.py
@@ -8,12 +8,13 @@ class CarSpider(scrapy.Spider):
 
     def parse(self, response):
         for car in response.xpath("//div[@class='search-results']"):
-            make, model = car.xpath('div/div/p/em/text()').getall()
-            yield {
-                'make': make,
-                'model': model 
-            }
-
-        next_page = response.css('li.next a::attr("href")').get()
+            car_data = car.xpath('div/div/p/em/text()').getall()
+            # require data has exact form [make, model]
+            if len(car_data) == 2:
+                yield {
+                    'make': car_data[0],
+                    'model': car_data[1]
+                }
+        next_page = response.xpath("//nav/ul/li/a[@aria-label='Next']/@href").get() 
         if next_page is not None:
             yield response.follow(next_page, self.parse)
