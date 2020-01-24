858dbc25e365d6bd3c366929c3adedff2c536543
chris <chrismarais@protonmail.com>
2020-01-24 10:01:48 +0200

    add car spider to scrape cars
    First working implementation - gets make and model of cars on
    automart.co.za

---
+++ car_spider.py
@@ -0,0 +1,19 @@
+import scrapy
+
+class QuotesSpider(scrapy.Spider):
+    name = 'quotes'
+    start_urls = [
+        'https://www.automart.co.za/used-cars/',
+    ]
+
+    def parse(self, response):
+        for car in response.xpath("//div[@class='search-results']"):
+            make, model = car.xpath('div/div/p/em/text()').getall()
+            yield {
+                'make': make,
+                'model': model 
+            }
+
+        next_page = response.css('li.next a::attr("href")').get()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
