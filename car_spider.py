6b637085deef76e3f940fd66c4a3fb9dc96ad328
chris <chrismarais@protonmail.com>
2020-01-27 14:32:27 +0200

    disentangle make/model logic from dealer_phone

--- car_spider.py
+++ car_spider.py
@@ -5,19 +5,19 @@ class CarSpider(scrapy.Spider):
     start_urls = [
         'https://www.automart.co.za/used-cars/',
     ]
-
+ 
     def parse(self, response):
-        for car in response.xpath("//div[@class='search-results']"):
-            make_model = car.xpath('div/div/p/em/text()').getall()
+        for result in response.xpath("//div[@class='search-results']"):
+            car = {}
+            make_model = result.xpath('div/div/p/em/text()').getall()
             # require data has exact form [make, model]
             if len(make_model) == 2:
-                yield {
-                    'make': make_model[0],
-                    'model': make_model[1],
-                    'dealer_phone':car.css("a#phnum::text").get()
-                }
+                car['make'] = make_model[0],
+                car['model'] = make_model[1],
             else:
                 self.logger.warning("Received incorrect data: %r" % make_model)
+            car['dealer_phone'] = result.css("a#phnum::text").get()
+            yield car
         next_page = response.xpath("//nav/ul/li/a[@aria-label='Next']/@href").get() 
         #if next_page is not None:
         #    yield response.follow(next_page, self.parse)
