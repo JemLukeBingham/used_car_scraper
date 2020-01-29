d082319ccd6d4e7fb39d30303b94ede1aaaf3401
chris <chrismarais@protonmail.com>
2020-01-29 13:37:27 +0200

    codefactor: trailing comma
    Solves Make/Model issue where data is singleton list!

--- car_spider.py
+++ car_spider.py
@@ -39,8 +39,8 @@ class CarSpider(scrapy.Spider):
             make_model = result.xpath('div/div/p/em/text()').getall()
             # require data has exact form [make, model]
             if len(make_model) == 2:
-                car['make'] = make_model[0],
-                car['model'] = make_model[1],
+                car['make'] = make_model[0]
+                car['model'] = make_model[1]
             else:
                 self.logger.warning("Received incorrect data: %r" % make_model)
             car['dealer_phone'] = result.css("a#phnum::text").get()
