0c24fb19501b0b692e6bce26f7d2d0cbb5717bf8
chris <chrismarais@protonmail.com>
2020-01-30 14:19:58 +0200

    seperate automart/gumtree spiders
    Seperate spider classes for gumtree & automart with text formatting
    functions moved to a separate module

--- gumtree_spider.py
+++ gumtree_spider.py
@@ -1,46 +1,23 @@
 import scrapy
-import logging
+from text_formatting import format_mileage, format_year, format_price
 
-def format_mileage(mileage):
-    """
-    Formats mileage text data from automart. Returns int in kilometres.
-    """
-    try:
-        return int(mileage.replace(" ", "").replace("km",""))
-    except (ValueError, TypeError):
-        logging.warning("Could not convert mileage data to int: %s" % mileage)
-        return mileage
-
-def format_year(year):
-    """
-    Formats year text data from automart. Returns int.
-    """
-    try:
-        return int(year)
-    except (ValueError, TypeError):
-        logging.warning("Could not convert year data to int: %r" % year)
-        return year
-
-def format_price(price):
-    if price is None:
-        return price
-    try:
-        return float(price.replace(" ", "").replace(",","").replace("R",""))
-    except (ValueError, TypeError):
-        logging.warning("Could not convert price data to float: %s" % price)
-        return price
-
-class CarSpider(scrapy.Spider):
-    name = 'cars'
+class GumtreeSpider(scrapy.Spider):
+    name = 'gumtree'
     start_urls = [
         'https://www.gumtree.co.za/s-cars-bakkies/v1c9077p1',
     ]
     def parse(self, response):
-        for result in response.xpath("//div[@class='view']/div[@id='srpAds']/div[@class='related-items']/div[@class='related-content']/div/div[@class='related-ad-content']"):
+        for result in response.xpath("//div[@class='view']/\
+                                        div[@id='srpAds']/\
+                                        div[@class='related-items']/\
+                                        div[@class='related-content']/\
+                                        div/div[@class='related-ad-content']"):
             car = {}
             price = result.xpath("div[@class='price']/span/span[@class='ad-price']/text()").get()
             car['price'] = format_price(price)
-            car['description'] = result.xpath("div[@class='description-content']/span[@class='related-ad-description']/span[@class='description-text']/text()").get()
+            car['description'] = result.xpath("div[@class='description-content']/\
+                                               span[@class='related-ad-description']/\
+                                               span[@class='description-text']/text()").get()
             yield car
         #next_page = response.xpath("//nav/ul/li/a[@aria-label='Next']/@href").get() 
         #if next_page is not None:
